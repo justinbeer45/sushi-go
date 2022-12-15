46b85a462f201ddfd7e2e6960344540d0964e084
Justin Wheeler <justin.wheeler0914@gmail.com>
2022-12-15 15:41:24 -0700

    passing hands and scoring - create pass_hand method

--- Player.py
+++ Player.py
@@ -1,8 +1,23 @@
 import Card
 
+def pass_hand(players):
+    player_num = len(players)
+    last_player_hand = players[player_num - 1].hand
+    print("Player {}'s hand:".format(player_num))
+    players[player_num - 1].show_hand()
+    for i in range(player_num - 1):
+        players[i + 1].hand = players[i].hand
+    players[0].hand = last_player_hand
+    for j in range(player_num):
+        print("Player {}'s hand:".format(j + 1))
+        players[j].show_hand()
+
+
+
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, number):
         self.name = name
+        self.number = number
         self.cards = []
         self.hand = []
         self.score = 0
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ print("\nWelcome, you can play SushiGo with 2-5 players")
 player_number = int(input("\nPlease enter the number of players:"))
 players = []
 for i in range(player_number):
-    players.append(Player.Player("Player {}".format(i + 1)))
+    players.append(Player.Player("Player {}".format(i + 1), i + 1))
 
 for player in players:
     print(player.name)
@@ -30,10 +30,6 @@ if player_number == 2:
         for player in players:
             player.hand.append(new_deck.deck[0])
             del new_deck.deck[0]
-print("{} cards in the deck now".format(len(new_deck.deck)))
-for player in players:
-    print()
-    player.show_hand()
 if player_number == 3:
     for i in range(9):
         for player in players:
@@ -49,5 +45,20 @@ if player_number == 5:
         for player in players:
             player.hand.append(new_deck.deck[0])
             del new_deck.deck[0]
+
+
+print("{} cards in the deck now".format(len(new_deck.deck)))
+for player in players:
+    print("\nPlayer {}'s hand for round 1".format(player.number))
+    player.show_hand()
+
+Player.pass_hand(players)
+
+for player in players:
+    print("\nPlayer {}'s hand after passing".format(player.number))
+    player.show_hand()
+
+
+
 
 
